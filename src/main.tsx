62ec4e1561d18c910cbb7543b6ba4454e9d09ec3
luciano victor <lucianocarvallho7@gmail.com>
2022-07-07 16:52:14 -0300

    feat: adding config styles sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import styles from './styles/home.module.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className={styles.container}></div>
   </React.StrictMode>
 )
